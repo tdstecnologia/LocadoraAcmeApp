f6767a15c8b9fe990c49b08113130c2eb226b78d
dherkyan <dherkyan@gmail.com>
2019-06-17 01:43:13 -0400

    Removendo pagina de erro padrao

--- Startup.cs
+++ Startup.cs
@@ -71,12 +71,6 @@ namespace LocadoraAcmeApp
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
