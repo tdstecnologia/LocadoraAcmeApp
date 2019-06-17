b2e2fdfa17ea4c76b74ecddfe89935ec327b77f9
dherkyan <dherkyan@gmail.com>
2019-06-17 07:49:19 -0400

    mapeamento Aluguel

---
+++ Mapeamento/AluguelMap.cs
@@ -0,0 +1,27 @@
+﻿using LocadoraAcmeApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.Mapeamento
+{
+    public class AluguelMap : IEntityTypeConfiguration<Aluguel>
+    {
+        public void Configure(EntityTypeBuilder<Aluguel> builder)
+        {
+            builder.HasKey(a => a.AluguelId);
+
+            builder.Property(a => a.Inicio).IsRequired();
+            builder.Property(a => a.Inicio).IsRequired();
+            builder.Property(a => a.PrecoTotal).IsRequired();
+
+            builder.HasOne(x => x.Usuario).WithMany(x => x.Alugueis).HasForeignKey(x => x.UsuarioId);
+            builder.HasOne(x => x.Carro).WithMany(x => x.Alugueis).HasForeignKey(x => x.CarroId);
+
+            builder.ToTable("tb06_aluguel");
+        }
+    }
+}
