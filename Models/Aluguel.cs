134d36e728213e038f6f2aaccf6c0b1faaafb49e
dherkyan <dherkyan@gmail.com>
2019-06-17 03:53:16 -0400

    classe Aluguel

---
+++ Models/Aluguel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.Models
+{
+    public class Aluguel
+    {
+        public int AluguelId { get; set; }
+
+        public string UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public string CarroId { get; set; }
+
+        public Carro Carro { get; set; }
+
+        public string Inicio { get; set; }
+
+        public string Fim { get; set; }
+
+        public int PrecoTotal { get; set; }
+    }
+}
