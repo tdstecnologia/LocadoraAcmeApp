4bed74f8a83df37778b0bdbf94a15465683b9eb6
dherkyan <dherkyan@gmail.com>
2019-06-17 03:38:15 -0400

    Classe Conta

---
+++ Models/Conta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.Models
+{
+    public class Conta
+    {
+        public int ContaId { get; set; }
+
+        public string UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public int Saldo { get; set; }
+    }
+}
