a0ec4f09a58e1fd7a9cb41849e14e0e6bef1112d
dherkyan <dherkyan@gmail.com>
2019-06-17 04:20:14 -0400

    Definindo os atributos de contexto

--- Models/Contexot.cs
+++ Models/Contexot.cs
@@ -8,6 +8,13 @@ namespace LocadoraAcmeApp.Models
 {
     public class Contexto : IdentityDbContext
     {
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<NiveisAcesso> niveisAcessos { get; set; }
+        public DbSet<Conta> Contas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Carro> Carros { get; set; }
+        public DbSet<Aluguel> Alugueis { get; set; }
         public Contexto(DbContextOptions<Contexto> options)
             : base(options)
         {
