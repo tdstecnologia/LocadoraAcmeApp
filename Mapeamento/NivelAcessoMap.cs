66af254f3e6538d7e63235eb003fcc4fddaeeab3
dherkyan <dherkyan@gmail.com>
2019-06-17 05:02:07 -0400

    mapeamento classe NivelAcesso

---
+++ Mapeamento/NivelAcessoMap.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraAcmeApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.Mapeamento
+{
+    public class UsuarNivelAcessoMapioMap : IEntityTypeConfiguration<NiveisAcesso>
+    {
+        public void Configure(EntityTypeBuilder<NiveisAcesso> builder)
+        {
+            builder.Property(n => n.Descricao).IsRequired().HasMaxLength(400);
+
+            builder.ToTable("tb02_nivel_acesso");
+        }
+    }
+}
