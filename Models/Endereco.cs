1a5aeec7e3bd331c974e651b80e3d9a76d417371
dherkyan <dherkyan@gmail.com>
2019-06-17 03:43:12 -0400

    classe Endereco

---
+++ Models/Endereco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.Models
+{
+    public class Endereco
+    {
+        public int EnderecoId { get; set; }
+
+        public int Numero { get; set; }
+
+        public string Rua { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
+    }
+}
