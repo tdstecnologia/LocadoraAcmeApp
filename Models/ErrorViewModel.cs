58fe9b3b99488fdff4b3b1e39468af54f93d7a35
dherkyan <dherkyan@gmail.com>
2019-06-17 00:06:21 -0400

    Projeto Locadora Acme

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LocadoraAcmeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
