c0296c96bc152a7de862d3850161ba5ded7abc49
dherkyan <dherkyan@gmail.com>
2019-06-17 07:34:00 -0400

    mapeamento classe Endereco

---
+++ Mapeamento/EnderecoMap.cs
@@ -0,0 +1,29 @@
+﻿using LocadoraAcmeApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.Mapeamento
+{
+    public class EnderecoMap : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.HasKey(e => e.EnderecoId);
+
+            builder.Property(e => e.Numero).IsRequired();
+            builder.Property(e => e.Rua).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Bairro).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Cidade).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Estado).IsRequired().HasMaxLength(100);
+
+
+            builder.HasOne(c => c.Usuario).WithMany(c => c.Enderecos).HasForeignKey(c => c.UsuarioId);
+
+            builder.ToTable("tb04_endereco");
+        }
+    }
+}
