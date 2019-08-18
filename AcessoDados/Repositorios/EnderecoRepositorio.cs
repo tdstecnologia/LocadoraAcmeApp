534f5fa1e36b0218b869f1c75a8b28b39d873965
dherkyan <dherkyan@gmail.com>
2019-08-18 00:47:22 -0400

    add classe impl IEnderecoRepositorio

---
+++ AcessoDados/Repositorios/EnderecoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using LocadoraAcmeApp.AcessoDados.Interfaces;
+using LocadoraAcmeApp.Models;
+
+namespace LocadoraAcmeApp.AcessoDados.Repositorios
+{
+    public class EnderecoRepositorio : RepositorioGenerico<Endereco>, IEnderecoRepositorio
+    {
+        public EnderecoRepositorio(Contexto contexto) : base(contexto)
+        {
+        }
+    }
+}
