25bd76a30ae717070e38b3bf3698763a3197ca42
dherkyan <dherkyan@gmail.com>
2019-08-18 05:35:01 -0400

    add CarrosAlugadosViewComponent

---
+++ ViewComponents/CarrosAlugadosViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using LocadoraAcmeApp.AcessoDados.Interfaces;
+using LocadoraAcmeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.ViewComponents
+{
+    public class CarrosAlugadosViewComponent : ViewComponent
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly Contexto _contexto;
+
+        public CarrosAlugadosViewComponent(IUsuarioRepositorio usuarioRepositorio, Contexto contexto)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var usuarioLogado = await _usuarioRepositorio.PegarUsuarioLogado(HttpContext.User);
+
+            return View(await _contexto.Alugueis.Include(a => a.Carro).Where(a => a.UsuarioId == usuarioLogado.Id).ToListAsync());
+        }
+    }
+}
