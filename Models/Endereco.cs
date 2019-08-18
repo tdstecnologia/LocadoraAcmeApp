89093b6a4849034661d8dbe19d687881800e4970
dherkyan <dherkyan@gmail.com>
2019-08-18 02:22:32 -0400

    Add validação do formulario

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,24 @@ namespace LocadoraAcmeApp.Models
     {
         public int EnderecoId { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Valor inválido")]
         public int Numero { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(100, ErrorMessage = "Use menos caracteres")]
         public string Rua { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
         public string Bairro { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
         public string Cidade { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
         public string Estado { get; set; }
 
         public string UsuarioId { get; set; }
