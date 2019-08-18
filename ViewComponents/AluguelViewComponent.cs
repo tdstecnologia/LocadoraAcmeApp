bb6550138d92d75ed3ff4ee31896c24c02da000e
dherkyan <dherkyan@gmail.com>
2019-08-18 04:57:40 -0400

    add AluguelViewComponent

---
+++ ViewComponents/AluguelViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using LocadoraAcmeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace LocadoraAcmeApp.ViewComponents
+{
+    public class AluguelViewComponent : ViewComponent
+    {
+        private readonly Contexto _contexto;
+
+        public AluguelViewComponent(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int CarroId)
+        {
+            return View(await _contexto.Carros.FirstOrDefaultAsync(c => c.CarroId == CarroId));
+        }
+    }
+}
